98b668a4ca394d647c8bbbf0634d39d4ed73fcbe
giridharkella <kgiridhars@gmail.com>
2026-02-03 10:48:41 -0500

    Initial commit

---
+++ accounts/src/main/java/org/gsk/bankapp/accounts/App.java
@@ -0,0 +1,13 @@
+package org.gsk.bankapp.accounts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
